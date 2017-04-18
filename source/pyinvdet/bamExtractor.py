9bb1b157c79f5a6f8bd6ddc6e6b1087a153fae32
zijuexiansheng <zijuexiansheng@gmail.com>
2017-04-18 16:16:11 -0400

    finished preprocessing modules

--- source/pyinvdet/bamExtractor.py
+++ source/pyinvdet/bamExtractor.py
@@ -5,19 +5,56 @@ import argparse
 import os
 import pysam
 
+### def print_aln(aln):
+###     print "the alignment range is 0-based, and [ , )"
+###     print
+###     print ">>> raw alignment data"
+###     print aln
+### 
+###     print "=" * 80
+###     print ">>> cigar string"
+###     print aln.cigarstring
+###     print ">>> mapping quality = ", aln.mapping_quality
+### 
+###     print "=" * 40 + " query " + "="*40
+###     print ">>> query_name =", aln.query_name
+###     print ">>> is reverse = ", aln.is_reverse
+###     print ">>> flag = ", aln.flag
+###     #print ">>> inferred read length from CIGAR string =", aln.infer_query_length(False)
+###     print ">>> query length =", aln.query_length
+###     print ">>> query length before partitioned into 3 parts =", aln.query_name.split('/', 1)[0].split('_')[-1]
+###     print ">>> query alignment pos"
+###     print "[{}, {}): {}".format(aln.query_alignment_start, aln.query_alignment_end, aln.query_alignment_length)
+### 
+###     print "=" * 40 + " reference " + "=" * 40
+###     print ">>> reference id =", aln.reference_id
+###     print ">>> reference alignment pos"
+###     print "[{}, {}): {}".format(aln.reference_start, aln.reference_end, aln.reference_length)
+
 def run(args):
     fin_bam = pysam.AlignmentFile(args.bam, "rb")
-    print os.path.join(args.directory + "simple_delta")
-    fout = open(os.path.join(args.directory, "simple_delta"), "w") if args.directory else sys.stdout
+    fout = open(os.path.join(args.directory, "brief_alignment"), "w") if args.directory else sys.stdout
     # write total # of references
     fout.write("{}\n".format( fin_bam.nreferences ))
     # write reference name: <length> <reference name>
     lengths = fin_bam.lengths
     for i in xrange(fin_bam.nreferences):
         fout.write("{} {}\n".format( lengths[i], fin_bam.get_reference_name(i) ))
+    # write simple alignment
+    for aln in fin_bam.fetch(until_eof = True):
+        qname = aln.query_name
+        if args.modify_qname:
+            qname = qname.split("/", 1)[0]
+            qname_tokens = qname.split("_")
+            qname = "{}/{}/0_{}".format(qname, qname_tokens[0][5:], int(qname_tokens[2])-1)
+        fout.write("{} {} {} ".format(aln.reference_id, qname, aln.query_length))
+        fout.write("{} {} {} {} {} {}\n".format(aln.reference_start, aln.reference_end, 
+                aln.query_alignment_start, aln.query_alignment_end,
+                aln.mapping_quality, 'R' if aln.is_reverse else 'F'))
     # write each alignment header
     if args.directory:
         fout.close()
+    fin_bam.close()
 
 
 
@@ -27,6 +64,7 @@ def parse_args( argv = None ):
     #parser.add_argument("-t", "--target", required=True, help="contig/scaffold file (fasta/q)")
     #parser.add_argument("-r", "--read", required=True, help="long reads file (fasta/q)")
     parser.add_argument("-d", "--directory", help="directory of the output file. If not set, it will be output to stdout")
+    parser.add_argument("-m", "--modify-qname", action="store_true", help="modify query name")
     return parser.parse_args( argv )
 
 def main( argv = None ):
@@ -35,4 +73,5 @@ def main( argv = None ):
     
 
 if __name__ == "__main__":
-    main(["-b", "/Volumes/bioinfo/tmp/finding_inversions/afun_out.ctg.bam", "-d", "."])
+    #main(["-b", "/Volumes/bioinfo/tmp/finding_inversions/afun_out.ctg.bam", "-d", ".", "-m"])
+    main()
---
+++ source/pyinvdet/fastqHeaderModifier.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+import argparse
+
+def parse_args( argv = None):
+    parser = argparse.ArgumentParser(description="change header of fastq files")
+    parser.add_argument("infile", help="input fastq file name")
+    parser.add_argument("outfile", help="output fastq file name")
+    return parser.parse_args( argv )
+
+def main( argv = None )
+    args = parse_args()
+    fin = open(args.infile, "r")
+    fout = open(args.outfile, "w")
+    fout_log = open(args.outfile + ".log", "w")
+    while True:
+        line = fin.readline()
+        if not line:    break
+        # generate new header
+        line = line.strip()
+        line_tokens = line.split('_')
+        new_header = "{}/{}/0_{}\n".format( line, line_tokens[0][5:], int(line_tokens[2]) - 1 )
+        fout.write(new_header)
+        fout_log.write("{}\n{}\n".format(line, new_header))
+
+        # write sequence
+        line = fin.readline()
+        fout.write( line )
+        # write +[tag]
+        line = fin.readline()
+        if line[1] == '\n' or line[1] == '\r':
+            fout.write( line )
+        else:
+            fout.write("+{}\n".format(new_header))
+        # write quality
+        line = fin.readline()
+        fout.write( line )
+    fin.close()
+    fout.close()
+    fout_log.close()
+
+
+if __name__ == "__main__":
+    main()
