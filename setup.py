6f2cb67d1e19506370ed7ace93ce154cb857a554
zijuexiansheng <zijuexiansheng@gmail.com>
2017-05-31 15:31:49 -0400

    new version schema following the pip standard

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import subprocess
 
 setup(
     name="invdet",
-    version="0.2."+subprocess.check_output(["git", "rev-parse", "HEAD"])[:7],
+    version=("0.2."+subprocess.check_output(["git", "rev-list", "--count", "f3e1a291253b5ac7ef2b1854adb879d115704d96..HEAD"])).rstrip(),
     description="Invertion Detection package",
     author="zijuexiansheng",
     license="MIT",
